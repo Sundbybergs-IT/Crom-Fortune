177fb28fa43292ad7403ff631043c0003b86fe5c
alwa <alix.warnke@gmail.com>
2022-01-24 22:43:07 +0100

    Fixed bug where it was possible to enter invalid quantities. Also made quantity an integer field (as is defined in the domain model).

--- domain/src/main/java/com/sundbybergsit/cromfortune/domain/StockSplit.kt
+++ domain/src/main/java/com/sundbybergsit/cromfortune/domain/StockSplit.kt
@@ -1,10 +1,22 @@
 package com.sundbybergsit.cromfortune.domain
 
+import androidx.annotation.IntRange
 import kotlinx.serialization.Serializable
 
 // Name == Stock symbol
 @Serializable
-data class StockSplit(val reverse: Boolean, val dateInMillis: Long, val name: String, val quantity: Int) {
+data class StockSplit(
+    val reverse: Boolean,
+    val dateInMillis: Long,
+    val name: String,
+    @IntRange(from = MIN_QUANTITY.toLong(), to = Int.MAX_VALUE.toLong()) val quantity: Int
+) {
+
+    companion object {
+
+        const val MIN_QUANTITY = 2
+
+    }
 
     fun toStockEvent(): StockEvent {
         return StockEvent(null, this, dateInMillis)
