7d7a4a9d16ff37b60bf560327f8577fb4d704bbd
alwa <alix.warnke@gmail.com>
2022-01-26 23:24:10 +0100

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.sundbybergsit.cromfortune"
         minSdk = 29
         targetSdk = 31
-        versionCode = 94
+        versionCode = 95
         versionName = baseVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
