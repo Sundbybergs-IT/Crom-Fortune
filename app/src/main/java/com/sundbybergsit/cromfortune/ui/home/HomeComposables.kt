f8910fc9efce8e9e631ef68fa5db8b20241da62a
alwa <alix.warnke@gmail.com>
2022-06-05 16:27:07 +0200

    More work on the dialog

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/home/HomeComposables.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/home/HomeComposables.kt
@@ -1,19 +1,30 @@
 package com.sundbybergsit.cromfortune.ui.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.google.accompanist.pager.HorizontalPager
 import com.sundbybergsit.cromfortune.R
+import com.sundbybergsit.cromfortune.contentDescription
+import java.time.Instant
 
 @Composable
 fun Home(viewModel: HomeViewModel) {
@@ -72,7 +83,10 @@ private fun StockOrderAggregates(modifier: Modifier, title: String, fabActive: B
                     bottom.linkTo(parent.bottom)
                 }
                 .padding(16.dp), onClick = { showDialog.value = true }) {
-                Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = "Floating Action Button Icon")
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_add),
+                    contentDescription = "Floating Action Button Icon"
+                )
             }
         }
     }
@@ -82,27 +96,139 @@ private fun StockOrderAggregates(modifier: Modifier, title: String, fabActive: B
 fun RegisterBuyStockAlertDialog(showDialog: Boolean, onConfirm: () -> Unit, onDismiss: () -> Unit) {
     // FIXME: Convert RegisterBuyStockDialogFragment into a composable
     if (showDialog) {
-        AlertDialog(
-            onDismissRequest = onDismiss,
-            title = {
-                Text(stringResource(id = R.string.action_stock_buy))
-            },
-            confirmButton = {
-                Button(onClick = onConfirm) {
-                    Text(stringResource(id = R.string.action_ok))
+        Dialog(onDismissRequest = onDismiss) {
+            ConstraintLayout {
+                val (titleRef, dateRef, currencyRef, amountRef, nameRef, priceRef, courtageRef, buttonsRef) = createRefs()
+                Text(modifier = Modifier.constrainAs(titleRef) {
+                    top.linkTo(parent.top)
+                }, text = stringResource(id = R.string.action_stock_buy))
+                val dateMutableState: MutableState<TextFieldValue> = remember {
+                    mutableStateOf(
+                        TextFieldValue(text = Instant.ofEpochMilli(System.currentTimeMillis()).toString())
+                    )
                 }
-            },
-            dismissButton = {
-                Button(
-                    onClick = onDismiss,
-                ) {
-                    Text(stringResource(id = R.string.action_cancel))
+                val dateErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                val dateErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                InputValidatedOutlinedTextField(
+                    modifier = Modifier
+                        .constrainAs(dateRef) {
+                            top.linkTo(titleRef.bottom)
+                            start.linkTo(parent.start)
+                        }
+                        .background(color = MaterialTheme.colors.background)
+                        .fillMaxWidth(),
+                    value = dateMutableState,
+                    isError = dateErrorMutableState.value,
+                    errorMessage = dateErrorMessageMutableState.value,
+                    contentDescriptor = "Date Input Text"
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .constrainAs(currencyRef) {
+                            top.linkTo(titleRef.bottom)
+                            start.linkTo(dateRef.start)
+                        }
+                        .background(color = MaterialTheme.colors.background)
+                        .fillMaxWidth(),
+                    onValueChange = {},
+                    enabled = false,
+                    value = ""
+                )
+                val amountMutableState: MutableState<TextFieldValue> =
+                    remember { mutableStateOf(TextFieldValue(text = "")) }
+                val amountErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                val amountErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                InputValidatedOutlinedTextField(
+                    modifier = Modifier
+                        .constrainAs(amountRef) {
+                            top.linkTo(dateRef.bottom)
+                            start.linkTo(parent.start)
+                        }
+                        .background(color = MaterialTheme.colors.background)
+                        .fillMaxWidth(),
+                    value = amountMutableState,
+                    isError = amountErrorMutableState.value,
+                    errorMessage = amountErrorMessageMutableState.value,
+                    contentDescriptor = "Amount Input Text"
+                )
+                // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
+                Row(modifier = Modifier
+                    .padding(all = 16.dp)
+                    .constrainAs(buttonsRef) {
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }) {
+
                 }
-            },
-            text = {
-                Text("This is a text on the dialog")
-            },
-        )
+            }
+        }
+    }
+}
+
+@Composable
+private fun InputValidatedOutlinedTextField(
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    value: MutableState<TextFieldValue>,
+    label: @Composable() (() -> Unit)? = null,
+    contentDescriptor: String,
+    errorMessage: String,
+    isError: Boolean,
+    horizontalPadding: Dp = 28.dp
+) {
+    Column(
+        modifier = modifier.padding(horizontal = horizontalPadding)
+    ) {
+        val shouldInputBeHighlighted = remember { mutableStateOf(false) }
+        Box(
+            Modifier
+                .onFocusChanged {
+                    shouldInputBeHighlighted.value = it.hasFocus
+                }
+                .background(
+                    color = if (shouldInputBeHighlighted.value && !isError) {
+                        MaterialTheme.colors.primarySurface
+                    } else {
+                        MaterialTheme.colors.background
+                    }, shape = MaterialTheme.shapes.small
+                )
+                .padding(4.dp)
+                .clip(MaterialTheme.shapes.small)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colors.background)
+                    .fillMaxWidth()
+                    .contentDescription(contentDescriptor),
+                singleLine = true,
+                value = value.value,
+                isError = isError,
+                onValueChange = { newText: TextFieldValue ->
+                    value.value = newText
+                },
+                textStyle = MaterialTheme.typography.body1,
+                label = label,
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                colors = TextFieldDefaults.outlinedTextFieldColors(unfocusedBorderColor = MaterialTheme.colors.onSurface),
+                trailingIcon = if (isError) {
+                    {
+                        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+                    }
+                } else {
+                    null
+                }
+            )
+        }
+        if (isError) {
+            Text(
+                modifier = Modifier.padding(start = 4.dp, top = 4.dp),
+                text = errorMessage,
+                style = MaterialTheme.typography.caption,
+                color = MaterialTheme.colors.error
+            )
+        }
     }
 }
 
