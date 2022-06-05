0828c926d8e4161f82b8709b367ec4fa00560b3c
alwa <alix.warnke@gmail.com>
2022-06-05 16:55:43 +0200

    More work on the dialog

---
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/ButtonComposables.kt
@@ -0,0 +1,26 @@
+package com.sundbybergsit.cromfortune.ui
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+private val buttonHeightInDp = 44.dp
+
+@Composable
+internal fun DialogButton(modifier : Modifier = Modifier, text : String, onClick : () -> Unit) {
+    Button(
+        modifier = modifier.height(buttonHeightInDp), onClick = onClick, elevation = null
+    ) {
+        ButtonText(text = text)
+    }
+}
+
+@Composable
+internal fun ButtonText(text: String, color: Color = Color.Unspecified) {
+    Text(text = text, style = MaterialTheme.typography.button, color = color)
+}
