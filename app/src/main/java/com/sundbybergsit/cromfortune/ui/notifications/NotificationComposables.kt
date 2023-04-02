bad7227de372d92e2300219e8c826599bb27f5b4
alwa <alix.warnke@gmail.com>
2023-04-02 16:58:46 +0200

    Work on NotificationComposables.kt

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/notifications/NotificationComposables.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/notifications/NotificationComposables.kt
@@ -1,45 +1,39 @@
 package com.sundbybergsit.cromfortune.ui.notifications
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.core.os.ConfigurationCompat
 import com.sundbybergsit.cromfortune.R
 import com.sundbybergsit.cromfortune.domain.notifications.NotificationMessage
 import com.sundbybergsit.cromfortune.ui.ButtonText
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
-fun Notifications(
-    viewModel: NotificationsViewModel, onBack: () -> Unit,
-    newNotificationsMutableState: MutableState<Collection<NotificationMessage>> =
-        remember {
-            mutableStateOf(
-                emptyList()
-                // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
-//                viewModel.newNotifications
-            )
-        },
-    oldNotificationsMutableState: MutableState<Collection<NotificationMessage>> =
-        remember { mutableStateOf(
-            // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
-            emptyList()
-            //                viewModel.oldNotifcations
-        )
-        },
-) {
-    val context = LocalContext.current
+fun Notifications(viewModel: NotificationsViewModel, onBack: () -> Unit) {
     val selectedTabIndexMutableState = remember { mutableStateOf(0) }
+    val context = LocalContext.current
+    val formatter = SimpleDateFormat(
+        "yyyy-MM-dd HH:mm", ConfigurationCompat
+            .getLocales(context.resources.configuration).get(0)
+    )
     Scaffold(topBar = {
         TopAppBar(
             title = {
@@ -53,7 +47,7 @@ fun Notifications(
             actions = {
                 TextButton(
                     onClick = {
-                        viewModel.clearNotifications(context)
+                        viewModel.clearNotifications()
                     }
                 ) {
                     ButtonText(
@@ -69,8 +63,8 @@ fun Notifications(
                 .padding(it)
         ) {
             val tabs = listOf(
-                stringResource(id = R.string.notifications_new_title) to newNotificationsMutableState,
-                stringResource(id = R.string.notifications_old_title) to oldNotificationsMutableState,
+                stringResource(id = R.string.notifications_new_title) to viewModel.newNotifications,
+                stringResource(id = R.string.notifications_old_title) to viewModel.oldNotifications,
             )
             Column {
                 TabRow(selectedTabIndexMutableState.value) {
@@ -88,17 +82,101 @@ fun Notifications(
                     items(tabs[selectedTabIndexMutableState.value].second.value.size) { page ->
                         when (selectedTabIndexMutableState.value) {
                             0 -> {
-                                // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
-                                // New notifications
+                                val newNotification = viewModel.newNotifications.value.elementAt(page)
+                                NewNotificationsTab(
+                                    notification = newNotification,
+                                    backgroundColor = if (page % 2 == 0) {
+                                        MaterialTheme.colors.secondary
+                                    } else {
+                                        MaterialTheme.colors.background
+                                    }, formatter = formatter
+                                )
                             }
                             1 -> {
-                                // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
-                                // Old notifications
+                                val oldNotification = viewModel.oldNotifications.value.elementAt(page)
+                                OldNotificationsTab(
+                                    notification = oldNotification,
+                                    backgroundColor = if (page % 2 == 0) {
+                                        MaterialTheme.colors.secondary
+                                    } else {
+                                        MaterialTheme.colors.background
+                                    }, formatter = formatter
+                                )
                             }
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun NewNotificationsTab(
+    notification: NotificationMessage,
+    backgroundColor: Color,
+    formatter: SimpleDateFormat
+) {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .clickable {
+                Toast
+                    .makeText(context, R.string.generic_error_not_supported, Toast.LENGTH_LONG)
+                    .show()
+            }
+            .background(color = backgroundColor)
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp), horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f),
+                text = formatter.format(Date(notification.dateInMillis)),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.weight(0.5f))
+            Text(modifier = Modifier.weight(1f), text = notification.message)
+        }
+    }
+}
+
+@Composable
+fun OldNotificationsTab(notification: NotificationMessage, backgroundColor: Color, formatter: SimpleDateFormat) {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .clickable {
+                Toast
+                    .makeText(context, R.string.generic_error_not_supported, Toast.LENGTH_LONG)
+                    .show()
+            }
+            .background(color = backgroundColor)
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp), horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f),
+                text = formatter.format(Date(notification.dateInMillis)),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.weight(0.5f))
+            Text(modifier = Modifier.weight(1f), text = notification.message)
         }
     }
 }
--- app/src/main/java/com/sundbybergsit/cromfortune/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/notifications/NotificationsViewModel.kt
@@ -1,60 +1,59 @@
 package com.sundbybergsit.cromfortune.ui.notifications
 
-import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sundbybergsit.cromfortune.R
-import com.sundbybergsit.cromfortune.notifications.NotificationsRepositoryImpl
+import com.sundbybergsit.cromfortune.domain.notifications.NotificationMessage
+import com.sundbybergsit.cromfortune.domain.notifications.NotificationsRepository
 import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 
-class NotificationsViewModel : ViewModel() {
+class NotificationsViewModel(private val notificationsRepository: NotificationsRepository) : ViewModel() {
 
-    private val _newNotifications = MutableLiveData<NotificationsViewState>()
-    val newNotifications: LiveData<NotificationsViewState> = _newNotifications
-    private val _oldNotifications = MutableLiveData<NotificationsViewState>()
-    val oldNotifications: LiveData<NotificationsViewState> = _oldNotifications
+    private val _newNotifications : MutableState<Collection<NotificationMessage>> = mutableStateOf(listOf())
+    val newNotifications: State<Collection<NotificationMessage>> = _newNotifications
+    private val _oldNotifications : MutableState<Collection<NotificationMessage>> = mutableStateOf(listOf())
+    val oldNotifications: State<Collection<NotificationMessage>> = _oldNotifications
 
-
-    fun refreshNew(context: Context) {
+    init {
         viewModelScope.launch {
-            val notifications = NotificationsRepositoryImpl(context).list().filter { notificationMessage ->
+            val notifications = notificationsRepository.list().filter { notificationMessage ->
                 LocalDate.now().isEqual(Instant.ofEpochMilli(notificationMessage.dateInMillis).atZone(ZoneId.systemDefault()).toLocalDate()) }
-                    .sortedByDescending {
-                notificationMessage -> notificationMessage.dateInMillis }
+                .sortedByDescending {
+                        notificationMessage -> notificationMessage.dateInMillis }
             if (notifications.isEmpty()) {
-                _newNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
+                _newNotifications.value = listOf()
+//                _newNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
             } else {
-                _newNotifications.postValue(NotificationsViewState.HasNotifications(R.string.notifications_title,
-                        NotificationAdapterItemUtil.convertToAdapterItems(notifications)))
+                _newNotifications.value = notifications
+//                _newNotifications.postValue(NotificationsViewState.HasNotifications(R.string.notifications_title,
+//                        NotificationAdapterItemUtil.convertToAdapterItems(notifications)))
+            }
+
+            val notifications2 = notificationsRepository.list()
+                .filter { notificationMessage ->
+                    Instant.ofEpochMilli(notificationMessage.dateInMillis).atZone(ZoneId.systemDefault())
+                        .toLocalDate().isBefore(LocalDate.now(ZoneId.systemDefault()))
+                }.sortedByDescending { notificationMessage -> notificationMessage.dateInMillis }
+            if (notifications2.isEmpty()) {
+                _newNotifications.value = listOf()
+//                _oldNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
+            } else {
+                _oldNotifications.value = notifications2
+//                _oldNotifications.postValue(NotificationsViewState.HasNotifications(R.string.notifications_title,
+//                        NotificationAdapterItemUtil.convertToAdapterItems(notifications)))
             }
         }
     }
 
-    fun refreshOld(context: Context) {
-        viewModelScope.launch {
-            val notifications = NotificationsRepositoryImpl(context).list()
-                    .filter { notificationMessage ->
-                        Instant.ofEpochMilli(notificationMessage.dateInMillis).atZone(ZoneId.systemDefault())
-                                .toLocalDate().isBefore(LocalDate.now(ZoneId.systemDefault()))
-                    }.sortedByDescending { notificationMessage -> notificationMessage.dateInMillis }
-            if (notifications.isEmpty()) {
-                _oldNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
-            } else {
-                _oldNotifications.postValue(NotificationsViewState.HasNotifications(R.string.notifications_title,
-                        NotificationAdapterItemUtil.convertToAdapterItems(notifications)))
-            }
-        }
-    }
-
-    fun clearNotifications(context: Context) {
-        NotificationsRepositoryImpl(context).clear()
-        _oldNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
-        _newNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
+    fun clearNotifications() {
+        notificationsRepository.clear()
+//        _oldNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
+//        _newNotifications.postValue(NotificationsViewState.HasNoNotifications(R.string.generic_error_empty))
     }
 
 }
