b1a49261f0ccb7f51a5fc6b78e68ac70b763d8f7
alwa <alix.warnke@gmail.com>
2023-06-12 21:55:04 +0200

    https://github.com/Sundbybergs-IT/Crom-Fortune/issues/66

--- domain/src/main/java/com/sundbybergsit/cromfortune/domain/StockPrice.kt
+++ domain/src/main/java/com/sundbybergsit/cromfortune/domain/StockPrice.kt
@@ -1,6 +1,6 @@
 package com.sundbybergsit.cromfortune.domain
 
-import java.util.*
+import java.util.Currency
 
 data class StockPrice(val stockSymbol: String, val currency: Currency, val price: Double) {
 
@@ -24,23 +24,26 @@ data class StockPrice(val stockSymbol: String, val currency: Currency, val price
             Triple("GGG.V", "G6 Materials Corp.", "CAD"),
             Triple("GIGSEK.ST", "Gaming Innovation Group Inc.", "SEK"),
             Triple("SHB-A.ST", "Svenska Handelsbanken AB (publ)", "SEK"),
-            Triple("HIMX", "Himax Technologies, Inc.", "USD"),
+            Triple("HIMX", "Himax Technologies ADR", "USD"),
             Triple("IMMR", "Immersion Corporation", "USD"),
             Triple("INVE-B.ST", "Investor AB (publ)", "SEK"),
             Triple("IPCO.ST", "International Petroleum Corporation", "SEK"),
             Triple("LHA.F", "Deutsche Lufthansa AG", "EUR"),
             Triple("LPK.DE", "LPKF Laser & Electronics AG", "EUR"),
             Triple("MIPS.ST", "MIPS AB (publ)", "SEK"),
+            Triple("MOH.F", "LVMH Moët Hennessy - Louis Vuitton, Société Européenne", "EUR"),
             Triple("NAS.OL", "Norwegian Air Shuttle ASA", "NOK"),
             Triple("NOKIA-SEK.ST", "Nokia Corporation", "SEK"),
+            Triple("OXY", "Occidental Petroleum Corporation", "USD"),
             Triple("POLYG.ST", "Polygiene AB (publ.)", "SEK"),
             Triple("RKT", "Rocket Companies, Inc.", "USD"),
             Triple("SALT-B.ST", "SaltX Technology Holding AB", "SEK"),
             Triple("SAND.ST", "Sandvik AB", "SEK"),
             Triple("SAS.ST", "SAS AB (publ)", "SEK"),
+            Triple("SBB-B.ST", "Samhällsbyggnadsbolaget i Norden AB (publ)", "SEK"),
             Triple("SHOT.ST", "Scandic Hotels Group AB (publ)", "SEK"),
             Triple("SOLT.ST", "SolTech Energy Sweden AB (publ)", "SEK"),
-            Triple("SOS", "SOS Limited", "USD"),
+            Triple("SOS", "SOS ADR", "USD"),
             Triple("SWED-A.ST", "Swedbank AB (publ)", "SEK"),
             Triple("TANGI.ST", "Tangiamo Touch Technology AB (publ)", "SEK"),
             Triple("TSLA", "Tesla, Inc.", "USD"),
