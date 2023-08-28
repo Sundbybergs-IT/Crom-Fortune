aaba30fcf97a0e86024432103eb47ea0328e1f99
alwa <alix.warnke@gmail.com>
2023-08-28 23:02:57 +0200

    Implemented auto-complete for buying stocks

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/DialogComposables.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/DialogComposables.kt
@@ -10,14 +10,15 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material.primarySurface
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -28,10 +29,13 @@ import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.PopupProperties
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.sundbybergsit.cromfortune.R
 import com.sundbybergsit.cromfortune.contentDescription
@@ -52,7 +56,7 @@ fun RegisterSellStockAlertDialog(showDialog: Boolean, onDismiss: () -> Unit, onS
             val scrollState = rememberScrollState()
             ConstraintLayout(
                 modifier = Modifier
-                    .background(MaterialTheme.colors.background)
+                    .background(MaterialTheme.colorScheme.background)
                     .verticalScroll(state = scrollState)
             ) {
                 DialogButton(text = stringResource(id = R.string.action_cancel), onClick = onDismiss)
@@ -106,7 +110,7 @@ fun RegisterBuyStockAlertDialog(
             val scrollState = rememberScrollState()
             ConstraintLayout(
                 modifier = Modifier
-                    .background(MaterialTheme.colors.background)
+                    .background(MaterialTheme.colorScheme.background)
                     .verticalScroll(state = scrollState)
             ) {
                 val (titleRef, dateRef, stockQuantityRef, stockNameRef, stockPriceRef, stockCurrencyRef, commissionFeeRef, buttonsRef) = createRefs()
@@ -132,7 +136,7 @@ fun RegisterBuyStockAlertDialog(
                         top.linkTo(titleRef.bottom)
                         start.linkTo(parent.start)
                     }
-                    .background(color = MaterialTheme.colors.background)
+                    .background(color = MaterialTheme.colorScheme.background)
                     .fillMaxWidth(),
                     label = { Text(text = stringResource(id = R.string.home_add_stock_date_label)) },
                     value = dateMutableState,
@@ -149,9 +153,10 @@ fun RegisterBuyStockAlertDialog(
                         top.linkTo(dateRef.bottom)
                         start.linkTo(parent.start)
                     }
-                    .background(color = MaterialTheme.colors.background)
+                    .background(color = MaterialTheme.colorScheme.background)
                     .fillMaxWidth(),
                     label = { Text(text = stringResource(id = R.string.home_add_stock_quantity_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     value = stockQuantityMutableState,
                     isError = stockQuantityErrorMutableState.value,
                     errorMessage = stockQuantityErrorMessageMutableState.value,
@@ -166,16 +171,33 @@ fun RegisterBuyStockAlertDialog(
                         TextFieldValue(text = "")
                     )
                 }
-                // FIXME: Auto-complete for stock names
-                InputValidatedOutlinedTextField(modifier = Modifier
-                    .constrainAs(stockNameRef) {
-                        top.linkTo(stockQuantityRef.bottom)
-                        start.linkTo(parent.start)
-                    }
-                    .background(color = MaterialTheme.colors.background)
-                    .fillMaxWidth(),
+
+                val allStocks = ArrayList(StockPrice.SYMBOLS.map { pair -> "${pair.second} (${pair.first})" }
+                    .toList())
+                val dropDownOptionsMutableState = remember { mutableStateOf(listOf<String>()) }
+                val dropDownExpandedMutableState = remember { mutableStateOf(false) }
+
+                fun onValueChanged(value: TextFieldValue) {
+                    dropDownExpandedMutableState.value = true
+                    stockNameMutableState.value = value
+                    dropDownOptionsMutableState.value =
+                        allStocks.filter { it.startsWith(value.text) && it != value.text }.take(3)
+                }
+                InputValidatedOutlinedTextFieldWithDropdown(
+                    modifier = Modifier
+                        .constrainAs(stockNameRef) {
+                            top.linkTo(stockQuantityRef.bottom)
+                            start.linkTo(parent.start)
+                        }
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .fillMaxWidth(),
                     label = { Text(text = stringResource(id = R.string.home_add_stock_name_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
                     value = stockNameMutableState,
+                    onValueChanged = ::onValueChanged,
+                    onDismissRequest = { dropDownExpandedMutableState.value = false },
+                    dropDownExpandedMutableState = dropDownExpandedMutableState,
+                    dropdownList = dropDownOptionsMutableState.value,
                     isError = stockNameErrorMutableState.value,
                     errorMessage = stockNameErrorMessageMutableState.value,
                     contentDescriptor = "Stock Name Input Text",
@@ -187,7 +209,8 @@ fun RegisterBuyStockAlertDialog(
                                 currencyMutableState.value = TextFieldValue(find.third)
                             }
                         }
-                    })
+                    }
+                )
                 val priceMutableState: MutableState<TextFieldValue> =
                     remember { mutableStateOf(TextFieldValue(text = "")) }
                 val priceErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
@@ -197,10 +220,11 @@ fun RegisterBuyStockAlertDialog(
                         top.linkTo(stockNameRef.bottom)
                         start.linkTo(parent.start)
                     }
-                    .background(color = MaterialTheme.colors.background)
+                    .background(color = MaterialTheme.colorScheme.background)
                     .fillMaxWidth(),
                     label = { Text(text = stringResource(id = R.string.generic_price_per_stock)) },
                     value = priceMutableState,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     isError = priceErrorMutableState.value,
                     errorMessage = priceErrorMessageMutableState.value,
                     contentDescriptor = "Stock Price Input Text"
@@ -213,13 +237,14 @@ fun RegisterBuyStockAlertDialog(
                             top.linkTo(stockPriceRef.bottom)
                             start.linkTo(parent.start)
                         }
-                        .background(color = MaterialTheme.colors.background)
+                        .background(color = MaterialTheme.colorScheme.background)
                         .fillMaxWidth(),
                     label = { Text(text = stringResource(id = R.string.home_add_stock_currency_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
                     onValueChange = {},
                     enabled = false,
                     value = currencyMutableState.value,
-                    textStyle = MaterialTheme.typography.body1,
+                    textStyle = MaterialTheme.typography.bodyMedium,
                 )
                 val commissionFeeMutableState: MutableState<TextFieldValue> =
                     remember { mutableStateOf(TextFieldValue(text = "")) }
@@ -230,9 +255,10 @@ fun RegisterBuyStockAlertDialog(
                         top.linkTo(stockCurrencyRef.bottom)
                         start.linkTo(parent.start)
                     }
-                    .background(color = MaterialTheme.colors.background)
+                    .background(color = MaterialTheme.colorScheme.background)
                     .fillMaxWidth(),
                     label = { Text(text = stringResource(id = R.string.home_add_commission_fee_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     value = commissionFeeMutableState,
                     isError = commissionFeeErrorMutableState.value,
                     errorMessage = commissionFeeErrorMessageMutableState.value,
@@ -334,15 +360,15 @@ private fun InputValidatedOutlinedTextField(
                 }
                 .background(
                     color = if (shouldInputBeHighlighted.value && !isError) {
-                        MaterialTheme.colors.primarySurface
+                        MaterialTheme.colorScheme.surface
                     } else {
-                        MaterialTheme.colors.background
+                        MaterialTheme.colorScheme.background
                     }, shape = MaterialTheme.shapes.small
                 )
                 .padding(4.dp)
                 .clip(MaterialTheme.shapes.small)) {
             OutlinedTextField(modifier = Modifier
-                .background(color = MaterialTheme.colors.background)
+                .background(color = MaterialTheme.colorScheme.background)
                 .fillMaxWidth()
                 .contentDescription(contentDescriptor),
                 singleLine = true,
@@ -351,11 +377,11 @@ private fun InputValidatedOutlinedTextField(
                 onValueChange = { newText: TextFieldValue ->
                     value.value = newText
                 },
-                textStyle = MaterialTheme.typography.body1,
+                textStyle = MaterialTheme.typography.bodyMedium,
                 label = label,
                 keyboardOptions = keyboardOptions,
                 keyboardActions = keyboardActions,
-                colors = TextFieldDefaults.outlinedTextFieldColors(unfocusedBorderColor = MaterialTheme.colors.onSurface),
+                colors = TextFieldDefaults.outlinedTextFieldColors(unfocusedBorderColor = MaterialTheme.colorScheme.onSurface),
                 trailingIcon = if (isError) {
                     {
                         Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
@@ -368,8 +394,97 @@ private fun InputValidatedOutlinedTextField(
             Text(
                 modifier = Modifier.padding(start = 4.dp, top = 4.dp),
                 text = errorMessage,
-                style = MaterialTheme.typography.caption,
-                color = MaterialTheme.colors.error
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+    }
+}
+
+@Composable
+internal fun InputValidatedOutlinedTextFieldWithDropdown(
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    value: MutableState<TextFieldValue>,
+    label: @Composable (() -> Unit)? = null,
+    contentDescriptor: String,
+    errorMessage: String,
+    isError: Boolean,
+    horizontalPadding: Dp = 28.dp,
+    onFocusChanged: ((FocusState) -> Unit)? = null,
+    onValueChanged: (TextFieldValue) -> Unit,
+    onDismissRequest: () -> Unit,
+    dropDownExpandedMutableState: MutableState<Boolean>,
+    dropdownList: List<String>,
+) {
+    Column(
+        modifier = modifier.padding(horizontal = horizontalPadding)
+    ) {
+        val shouldInputBeHighlighted = remember { mutableStateOf(false) }
+        Box(
+            Modifier
+                .onFocusChanged {
+                    shouldInputBeHighlighted.value = it.hasFocus
+                    onFocusChanged?.invoke(it)
+                }
+                .background(
+                    color = if (shouldInputBeHighlighted.value && !isError) {
+                        MaterialTheme.colorScheme.surface
+                    } else {
+                        MaterialTheme.colorScheme.background
+                    }, shape = MaterialTheme.shapes.small
+                )
+                .padding(4.dp)
+                .clip(MaterialTheme.shapes.small)) {
+            OutlinedTextField(modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.background)
+                .fillMaxWidth()
+                .contentDescription(contentDescriptor),
+                singleLine = true,
+                value = value.value,
+                isError = isError,
+                onValueChange = onValueChanged,
+                textStyle = MaterialTheme.typography.bodyMedium,
+                label = label,
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                colors = TextFieldDefaults.outlinedTextFieldColors(unfocusedBorderColor = MaterialTheme.colorScheme.onSurface),
+                trailingIcon = if (isError) {
+                    {
+                        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+                    }
+                } else {
+                    null
+                })
+        }
+        DropdownMenu(
+            expanded = dropDownExpandedMutableState.value,
+            properties = PopupProperties(
+                focusable = false,
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            ),
+            onDismissRequest = onDismissRequest
+        ) {
+            dropdownList.forEach { text ->
+                DropdownMenuItem(onClick = {
+                    onValueChanged(
+                        TextFieldValue(
+                            text = text,
+                            selection = TextRange(text.length)
+                        )
+                    )
+                    dropDownExpandedMutableState.value = false
+                }, text = { Text(text = text) })
+            }
+        }
+        if (isError) {
+            Text(
+                modifier = Modifier.padding(start = 4.dp, top = 4.dp),
+                text = errorMessage,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.error
             )
         }
     }
