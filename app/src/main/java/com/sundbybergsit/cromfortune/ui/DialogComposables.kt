110fbcd6801352c33550cb0727a8939b35c4e718
alwa <alix.warnke@gmail.com>
2023-08-28 23:18:46 +0200

    Implemented sell dialog

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/DialogComposables.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/DialogComposables.kt
@@ -42,6 +42,7 @@ import com.sundbybergsit.cromfortune.contentDescription
 import com.sundbybergsit.cromfortune.domain.StockOrder
 import com.sundbybergsit.cromfortune.domain.StockPrice
 import com.sundbybergsit.cromfortune.domain.StockSplit
+import com.sundbybergsit.cromfortune.ui.home.HomeViewModel
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Currency
@@ -50,7 +51,12 @@ import java.util.Locale
 private const val DATE_FORMAT = "MM/dd/yyyy"
 
 @Composable
-fun RegisterSellStockAlertDialog(showDialog: Boolean, onDismiss: () -> Unit, onSave: (StockOrder) -> Unit) {
+fun RegisterSellStockAlertDialog(
+    showDialog: Boolean,
+    onDismiss: () -> Unit,
+    onSave: (StockOrder) -> Unit,
+    homeViewModel: HomeViewModel
+) {
     if (showDialog) {
         Dialog(onDismissRequest = onDismiss) {
             val scrollState = rememberScrollState()
@@ -59,34 +65,227 @@ fun RegisterSellStockAlertDialog(showDialog: Boolean, onDismiss: () -> Unit, onS
                     .background(MaterialTheme.colorScheme.background)
                     .verticalScroll(state = scrollState)
             ) {
-                DialogButton(text = stringResource(id = R.string.action_cancel), onClick = onDismiss)
-                val context = LocalContext.current
-                DialogButton(text = stringResource(id = android.R.string.ok), onClick = {
-                    // FIXME: Implement, https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
-                    try {
-//                        inputDate.validateDate(inputLayoutDate, DATE_FORMAT)
-//                        inputCurrency.validateCurrency(inputLayoutCurrency)
-//                        inputStockQuantity.validateInt(inputLayoutStockQuantity)
-//                        inputStockName.validateStockName(inputLayoutStockName)
-//                        val stockSymbol = inputStockName.text.toString().substringAfterLast('(')
-//                            .substringBeforeLast(')')
-//                        validateHasStockQuantity(stockSymbol, inputStockQuantity, inputLayoutStockQuantity)
-//                        inputStockPrice.validateDouble(inputLayoutStockPrice)
-//                        inputCommissionFee.validateDouble(inputLayoutCommissionFee)
-//                        val dateAsString = inputDate.text.toString()
-//                        val date = SimpleDateFormat(DATE_FORMAT, Locale.getDefault()).parse(dateAsString)!!
-//                        val currency = Currency.getInstance(inputCurrency.text.toString())
-                        // TODO: Convert commission fee (in SEK) to selected currency
-//                        val stockOrder = com.sundbybergsit.cromfortune.domain.StockOrder(
-//                            "Sell", currency.toString(), date.time, stockSymbol,
-//                            inputStockPrice.text.toString().toDouble(), inputCommissionFee.text.toString().toDouble(),
-//                            inputStockQuantity.text.toString().toInt()
-//                        )
-//                        onSave.invoke(stockOrder)
-                    } catch (e: ValidatorException) {
-                        // Shit happens ...
+                val (titleRef, dateRef, stockQuantityRef, stockNameRef, stockPriceRef, stockCurrencyRef, commissionFeeRef, buttonsRef) = createRefs()
+
+                Text(
+                    modifier = Modifier
+                        .padding(all = 16.dp)
+                        .constrainAs(titleRef) {
+                            top.linkTo(parent.top)
+                        }, text = stringResource(id = R.string.action_stock_sell)
+                )
+                val myCalendar = Calendar.getInstance()
+                val sdf = SimpleDateFormat(DATE_FORMAT, Locale.getDefault())
+                val dateMutableState: MutableState<TextFieldValue> = remember {
+                    mutableStateOf(TextFieldValue(text = sdf.format(myCalendar.time)))
+                }
+                val dateErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                val dateErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                InputValidatedOutlinedTextField(modifier = Modifier
+                    .constrainAs(dateRef) {
+                        top.linkTo(titleRef.bottom)
+                        start.linkTo(parent.start)
                     }
-                })
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .fillMaxWidth(),
+                    label = { Text(text = stringResource(id = R.string.home_add_stock_date_label)) },
+                    value = dateMutableState,
+                    isError = dateErrorMutableState.value,
+                    errorMessage = dateErrorMessageMutableState.value,
+                    contentDescriptor = "Date Input Text"
+                )
+                val stockQuantityMutableState: MutableState<TextFieldValue> =
+                    remember { mutableStateOf(TextFieldValue(text = "")) }
+                val stockQuantityErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                val stockQuantityErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                InputValidatedOutlinedTextField(modifier = Modifier
+                    .constrainAs(stockQuantityRef) {
+                        top.linkTo(dateRef.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .fillMaxWidth(),
+                    label = { Text(text = stringResource(id = R.string.home_add_stock_quantity_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    value = stockQuantityMutableState,
+                    isError = stockQuantityErrorMutableState.value,
+                    errorMessage = stockQuantityErrorMessageMutableState.value,
+                    contentDescriptor = "Stock Quantity Input Text"
+                )
+                val stockNameMutableState: MutableState<TextFieldValue> =
+                    remember { mutableStateOf(TextFieldValue(text = "")) }
+                val stockNameErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                val stockNameErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                val currencyMutableState: MutableState<TextFieldValue> = remember {
+                    mutableStateOf(
+                        TextFieldValue(text = "")
+                    )
+                }
+
+                val allStocks = ArrayList(StockPrice.SYMBOLS.map { pair -> "${pair.second} (${pair.first})" }
+                    .toList())
+                val dropDownOptionsMutableState = remember { mutableStateOf(listOf<String>()) }
+                val dropDownExpandedMutableState = remember { mutableStateOf(false) }
+
+                fun onValueChanged(value: TextFieldValue) {
+                    dropDownExpandedMutableState.value = true
+                    stockNameMutableState.value = value
+                    dropDownOptionsMutableState.value =
+                        allStocks.filter { it.startsWith(value.text) && it != value.text }.take(3)
+                }
+                InputValidatedOutlinedTextFieldWithDropdown(
+                    modifier = Modifier
+                        .constrainAs(stockNameRef) {
+                            top.linkTo(stockQuantityRef.bottom)
+                            start.linkTo(parent.start)
+                        }
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .fillMaxWidth(),
+                    label = { Text(text = stringResource(id = R.string.home_add_stock_name_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    value = stockNameMutableState,
+                    onValueChanged = ::onValueChanged,
+                    onDismissRequest = { dropDownExpandedMutableState.value = false },
+                    dropDownExpandedMutableState = dropDownExpandedMutableState,
+                    dropdownList = dropDownOptionsMutableState.value,
+                    isError = stockNameErrorMutableState.value,
+                    errorMessage = stockNameErrorMessageMutableState.value,
+                    contentDescriptor = "Stock Name Input Text",
+                    onFocusChanged = {
+                        if (!it.hasFocus) {
+                            val find =
+                                StockPrice.SYMBOLS.find { triple -> "${triple.second} (${triple.first})" == stockNameMutableState.value.text }
+                            if (find != null) {
+                                currencyMutableState.value = TextFieldValue(find.third)
+                            }
+                        }
+                    }
+                )
+                val priceMutableState: MutableState<TextFieldValue> =
+                    remember { mutableStateOf(TextFieldValue(text = "")) }
+                val priceErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                val priceErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                InputValidatedOutlinedTextField(modifier = Modifier
+                    .constrainAs(stockPriceRef) {
+                        top.linkTo(stockNameRef.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .fillMaxWidth(),
+                    label = { Text(text = stringResource(id = R.string.generic_price_per_stock)) },
+                    value = priceMutableState,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    isError = priceErrorMutableState.value,
+                    errorMessage = priceErrorMessageMutableState.value,
+                    contentDescriptor = "Stock Price Input Text"
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .padding(horizontal = 32.dp)
+                        .constrainAs(stockCurrencyRef) {
+                            top.linkTo(stockPriceRef.bottom)
+                            start.linkTo(parent.start)
+                        }
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .fillMaxWidth(),
+                    label = { Text(text = stringResource(id = R.string.home_add_stock_currency_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    onValueChange = {},
+                    enabled = false,
+                    value = currencyMutableState.value,
+                    textStyle = MaterialTheme.typography.bodyMedium,
+                )
+                val commissionFeeMutableState: MutableState<TextFieldValue> =
+                    remember { mutableStateOf(TextFieldValue(text = "")) }
+                val commissionFeeErrorMutableState: MutableState<Boolean> = remember { mutableStateOf(false) }
+                val commissionFeeErrorMessageMutableState: MutableState<String> = remember { mutableStateOf("") }
+                InputValidatedOutlinedTextField(modifier = Modifier
+                    .constrainAs(commissionFeeRef) {
+                        top.linkTo(stockCurrencyRef.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .fillMaxWidth(),
+                    label = { Text(text = stringResource(id = R.string.home_add_commission_fee_label)) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    value = commissionFeeMutableState,
+                    isError = commissionFeeErrorMutableState.value,
+                    errorMessage = commissionFeeErrorMessageMutableState.value,
+                    contentDescriptor = "Commission Fee Input Text"
+                )
+                Row(modifier = Modifier
+                    .padding(all = 16.dp)
+                    .constrainAs(buttonsRef) {
+                        top.linkTo(commissionFeeRef.bottom)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }) {
+                    DialogButton(text = stringResource(id = R.string.action_cancel), onClick = onDismiss)
+                    val context = LocalContext.current
+                    DialogButton(text = stringResource(id = android.R.string.ok), onClick = {
+                        try {
+                            dateMutableState.value.validateDate(
+                                context = context,
+                                errorMutableState = dateErrorMutableState,
+                                errorMessageMutableState = dateErrorMessageMutableState,
+                                pattern = DATE_FORMAT
+                            )
+                            stockQuantityMutableState.value.validateInt(
+                                context = context,
+                                errorMutableState = stockQuantityErrorMutableState,
+                                errorMessageMutableState = stockQuantityErrorMessageMutableState
+                            )
+                            stockQuantityMutableState.value.validateMinQuantity(
+                                context = context,
+                                errorMutableState = stockQuantityErrorMutableState,
+                                errorMessageMutableState = stockQuantityErrorMessageMutableState,
+                                minValue = 1
+                            )
+                            stockNameMutableState.value.validateStockName(
+                                context = context,
+                                errorMutableState = stockNameErrorMutableState,
+                                errorMessageMutableState = stockNameErrorMessageMutableState
+                            )
+                            val stockSymbol =
+                                stockNameMutableState.value.text.substringAfterLast('(').substringBeforeLast(')')
+                            stockQuantityMutableState.value.validateStockQuantity(
+                                context = context,
+                                errorMutableState = stockQuantityErrorMutableState,
+                                errorMessageMutableState = stockQuantityErrorMessageMutableState,
+                                stockName = stockSymbol,
+                                homeViewModel = homeViewModel
+                            )
+                            priceMutableState.value.validateDouble(
+                                context = context,
+                                errorMutableState = priceErrorMutableState,
+                                errorMessageMutableState = priceErrorMessageMutableState
+                            )
+                            commissionFeeMutableState.value.validateDouble(
+                                context = context,
+                                errorMutableState = commissionFeeErrorMutableState,
+                                errorMessageMutableState = commissionFeeErrorMessageMutableState
+                            )
+                            val dateAsString = dateMutableState.value.text
+                            val inputDate = SimpleDateFormat(DATE_FORMAT, Locale.getDefault()).parse(dateAsString)
+
+                            val currency = Currency.getInstance(currencyMutableState.value.text)
+                            // TODO: Convert commission fee (in SEK) to selected currency
+                            val stockOrder = StockOrder(
+                                "Sell",
+                                currency.toString(),
+                                inputDate.time,
+                                stockSymbol,
+                                priceMutableState.value.text.toDouble(),
+                                commissionFeeMutableState.value.text.toDouble(),
+                                stockQuantityMutableState.value.text.toInt()
+                            )
+                            onSave.invoke(stockOrder)
+                            onDismiss.invoke()
+                        } catch (e: ValidatorException) {
+                            // Shit happens ...
+                        }
+                    })
+                }
             }
         }
     }
