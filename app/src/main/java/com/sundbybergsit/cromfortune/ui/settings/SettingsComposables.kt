669e698febe3c85d7c417056c5d001847671c772
alwa <alix.warnke@gmail.com>
2023-09-01 23:32:57 +0200

    Fixed Settings

--- app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsComposables.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/ui/settings/SettingsComposables.kt
@@ -2,7 +2,6 @@ package com.sundbybergsit.cromfortune.ui.settings
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -14,9 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.sundbybergsit.cromfortune.LeafScreen
 import com.sundbybergsit.cromfortune.OverflowMenu
@@ -49,18 +49,23 @@ fun Settings(viewModel: SettingsViewModel, onBack: () -> Unit, onNavigateTo: (St
                 .fillMaxSize()
                 .padding(it)
         ) {
-            Column(modifier = Modifier.fillMaxWidth()) {
+            val (titleRef, subtitleRef) = createRefs()
+            createVerticalChain(titleRef, subtitleRef, chainStyle = ChainStyle.Packed)
+            Column(modifier = Modifier
+                .constrainAs(titleRef) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }, horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     text = stringResource(id = R.string.settings_default_values),
                     style = MaterialTheme.typography.titleMedium,
-                    textAlign = TextAlign.Center
                 )
                 Text(
                     text = stringResource(id = R.string.generic_error_not_supported),
-                    style = MaterialTheme.typography.bodyMedium, textAlign = TextAlign.Center
+                    style = MaterialTheme.typography.bodyMedium,
                 )
-                // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
             }
+            // FIXME: https://github.com/Sundbybergs-IT/Crom-Fortune/issues/21
         }
     }
 }
