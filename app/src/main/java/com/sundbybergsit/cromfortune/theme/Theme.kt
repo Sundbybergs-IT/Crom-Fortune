867cf404d39a895327449f23818db5780dad29f1
alwa <alix.warnke@gmail.com>
2023-09-17 16:53:30 +0200

    Updated theme to look better

--- app/src/main/java/com/sundbybergsit/cromfortune/theme/Theme.kt
+++ app/src/main/java/com/sundbybergsit/cromfortune/theme/Theme.kt
@@ -69,20 +69,19 @@ private val DarkColors = darkColorScheme(
     outlineVariant = md_theme_dark_outlineVariant,
     scrim = md_theme_dark_scrim,
 )
-
 @Composable
 fun AppTheme(
-    useDarkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+  useDarkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable() () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
-    }
+  val colors = if (!useDarkTheme) {
+    LightColors
+  } else {
+    DarkColors
+  }
 
-    MaterialTheme(
-        colorScheme = colors,
-        content = content
-    )
+  MaterialTheme(
+    colorScheme = colors,
+    content = content
+  )
 }
