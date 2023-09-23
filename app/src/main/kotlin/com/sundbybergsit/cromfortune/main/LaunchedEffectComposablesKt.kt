e8b5436f890047fb70c509500a5ea8ffe268a27e
alwa <alix.warnke@gmail.com>
2023-09-23 23:23:01 +0200

    https://github.com/Sundbybergs-IT/Crom-Fortune/issues/77

--- app/src/main/kotlin/com/sundbybergsit/cromfortune/main/LaunchedEffectComposablesKt.kt
+++ app/src/main/kotlin/com/sundbybergsit/cromfortune/main/LaunchedEffectComposablesKt.kt
@@ -3,6 +3,12 @@ package com.sundbybergsit.cromfortune.main
 import android.os.Build
 import android.view.HapticFeedbackConstants
 import android.view.View
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TimePickerState
@@ -12,13 +18,58 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import com.sundbybergsit.cromfortune.domain.StockPrice
 import com.sundbybergsit.cromfortune.main.settings.StockRetrievalSettings
+import com.sundbybergsit.cromfortune.main.stocks.StockPriceRepository
+import com.sundbybergsit.cromfortune.main.ui.dashboard.DashboardViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import java.time.DayOfWeek
+
+@Composable
+internal fun RefreshFromViewStateLaunchedEffect(
+    viewState: StockPriceRepository.ViewState?,
+    viewModel: DashboardViewModel
+) {
+    val context = LocalContext.current
+    LaunchedEffect(key1 = viewState) {
+        when (viewState) {
+            is StockPriceRepository.ViewState -> {
+                viewModel.refresh(
+                    context = context,
+                    timestamp = viewState.instant,
+                    stockPrices = viewState.stockPrices
+                )
+            }
+
+            else -> {
+                // Do nothing
+            }
+        }
+    }
+}
+
+@Composable
+internal fun AnimateRotationLaunchedEffect(
+    rotation: Animatable<Float, AnimationVector1D>,
+    currentRotationMutableState: MutableState<Float>,
+    durationInMs : Int,
+) {
+    LaunchedEffect(key1 = Unit) {
+        rotation.animateTo(
+            targetValue = currentRotationMutableState.value + 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationInMs, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            )
+        ) {
+            currentRotationMutableState.value = value
+        }
+    }
+}
 
 @Composable
 internal fun LoadValueFromParameterLaunchedEffect(
