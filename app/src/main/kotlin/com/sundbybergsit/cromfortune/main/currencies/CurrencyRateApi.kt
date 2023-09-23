54ac5a56336ca3cf7bdc15009dfe5ee83d968550
alwa <alix.warnke@gmail.com>
2023-09-23 18:04:47 +0200

    Refactoring
    Migrated type from State to StateFlow to get rid of Compose dependency as preparation to move CurrencyRateApi to different module

--- app/src/main/kotlin/com/sundbybergsit/cromfortune/main/currencies/CurrencyRateApi.kt
+++ app/src/main/kotlin/com/sundbybergsit/cromfortune/main/currencies/CurrencyRateApi.kt
@@ -1,12 +1,12 @@
 package com.sundbybergsit.cromfortune.main.currencies
 
-import androidx.compose.runtime.State
 import com.sundbybergsit.cromfortune.domain.currencies.CurrencyRate
 import com.sundbybergsit.cromfortune.main.currencies.CurrencyRateRepository.ViewState
+import kotlinx.coroutines.flow.StateFlow
 
 interface CurrencyRateApi {
 
-    val currencyRates: State<ViewState?>
+    val currencyRates: StateFlow<ViewState?>
 
     fun add(currencyRates: Set<CurrencyRate>)
 
--- app/src/main/kotlin/com/sundbybergsit/cromfortune/main/currencies/CurrencyRateRepository.kt
+++ app/src/main/kotlin/com/sundbybergsit/cromfortune/main/currencies/CurrencyRateRepository.kt
@@ -2,10 +2,9 @@ package com.sundbybergsit.cromfortune.main.currencies
 
 import android.util.Log
 import androidx.annotation.VisibleForTesting
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import com.sundbybergsit.cromfortune.domain.currencies.CurrencyRate
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import java.time.Instant
 
 object CurrencyRateRepository : CurrencyRateApi {
@@ -14,9 +13,9 @@ object CurrencyRateRepository : CurrencyRateApi {
 
     @VisibleForTesting
     @Suppress("ObjectPropertyName")
-    val _currencyRates: MutableState<ViewState?> = mutableStateOf(null)
+    val _currencyRates: MutableStateFlow<ViewState?> = MutableStateFlow(null)
 
-    override val currencyRates: State<ViewState?> = _currencyRates
+    override val currencyRates: StateFlow<ViewState?> = _currencyRates
 
     override fun add(currencyRates: Set<CurrencyRate>) {
         Log.v(TAG, "put(${currencyRates})")
