0762dcbdbbf6d3e86cbce2ec2b75ff6388014db1
alwa <alix.warnke@gmail.com>
2023-10-05 23:01:16 +0200

    Bumped version + cleanup

--- app/src/main/kotlin/com/sundbybergsit/cromfortune/main/MenusKt.kt
+++ app/src/main/kotlin/com/sundbybergsit/cromfortune/main/MenusKt.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     alias(libs.plugins.sonarqube)
 }
 
-val baseVersionName = "0.7.3"
+val baseVersionName = "0.7.4"
 
 allprojects {
 
