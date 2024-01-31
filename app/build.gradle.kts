222a76b4fd4114431a875ce893de21cee8a9d018
alwa <alix.warnke@gmail.com>
2024-01-31 23:58:58 +0100

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     namespace = "com.sundbybergsit.cromfortune.main"
     defaultConfig {
         applicationId = "com.sundbybergsit.cromfortune"
-        versionCode = 120
+        versionCode = 121
         versionName = baseVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     alias(libs.plugins.sonarqube)
 }
 
-val baseVersionName = "0.8.5"
+val baseVersionName = "0.8.6"
 
 allprojects {
 
