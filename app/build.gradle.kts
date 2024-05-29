417cccb59275b1a2b51137cccc169fa9f8436555
alwa <alix.warnke@gmail.com>
2024-05-29 22:17:12 +0200

    Test bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     namespace = "com.sundbybergsit.cromfortune.main"
     defaultConfig {
         applicationId = "com.sundbybergsit.cromfortune"
-        versionCode = 127
+        versionCode = 128
         versionName = baseVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
