4c6251037ec5453f0f406e3c6c7dbd890055eeb0
alwa <alix.warnke@gmail.com>
2024-05-29 18:16:21 +0200

    Bumped versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     namespace = "com.sundbybergsit.cromfortune.main"
     defaultConfig {
         applicationId = "com.sundbybergsit.cromfortune"
-        versionCode = 125
+        versionCode = 126
         versionName = baseVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
