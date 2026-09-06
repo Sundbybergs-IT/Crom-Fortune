cc7325eb0b61078c7b9184211ec6a72de5ac6c92
alwa <alix.warnke@gmail.com>
2026-09-06 10:30:35 +0200

    Misc updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     namespace = "com.sundbybergsit.cromfortune.main"
     defaultConfig {
         applicationId = "com.sundbybergsit.cromfortune"
-        versionCode = 142
+        versionCode = 143
         versionName = baseVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
