1b6029fda111a33b479f10a9267907359275534a
alwa <alix.warnke@gmail.com>
2026-09-10 18:20:00 +0200

    https://github.com/Sundbybergs-IT/Crom-Fortune/issues/132

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     namespace = "com.sundbybergsit.cromfortune.main"
     defaultConfig {
         applicationId = "com.sundbybergsit.cromfortune"
-        versionCode = 146
+        versionCode = 147
         versionName = baseVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
